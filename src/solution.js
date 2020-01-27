d1bf3f8ee2401d9000c55efdf49f2581d9929f10
Yernar <ymussin@opentext.com>
2020-01-27 18:47:31 +0600

    Solution.js added meethods for finding optimal path and distance

--- src/solution.js
+++ src/solution.js
@@ -1,3 +1,56 @@
 const solution = function(graph, start, finish)  {
-    // Ваше решение
+    const getStar = (value) => {
+        return stars.find(star => star.name === value);
+    };
+
+    const setFirstStar = (value) => {
+        let star = getStar(value);
+        star.distance = 0;
+        star.path = [START];
+    };
+
+    const getStarsFromGraph = (graph) => {
+        const stars = [];
+        Object.keys(graph).forEach(key => stars.push({
+            name: key,
+            distance: Infinity,
+            path: []
+       }));
+       return stars;
+    };
+
+    setDistancePath = (toStar, fromStar, key, value) => {
+        toStar.distance = fromStar.distance + value;
+        toStar.path = [];
+        fromStar.path.forEach(p => toStar.path.push(p));
+        toStar.path.push(key);
+    };
+
+    const findStar = (point, newPoints) => {
+        let fromStar = getStar(point);
+        Object.entries(graph[point]).forEach(([key, value]) => {
+            let toStar = getStar(key);
+            if (fromStar.distance + value < toStar.distance) {
+                setDistancePath(toStar, fromStar, key, value);
+                newPoints.push(key);
+            }
+        });
+        return newPoints;
+    }
+
+    const stars = getStarsFromGraph(graph);
+    
+    setFirstStar(START);
+
+    let points = [START];
+
+    while (getStar(FINISH).distance === Infinity) {
+        let newPoints = []
+        points.forEach(point => {
+            newPoints = findStar(point, newPoints);
+        });
+        points = newPoints.slice();
+    };
+    
+    return getStar(FINISH);
 }
